624b2a36794947c3a067bc59cba2145f4a9df693
Roman Dubovik <ip.romandubovik@gmail.com>
2023-07-09 22:52:32 +0300

    CollectinoView abstract class was created, UserList view class added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-import { User } from './models/User';
-import { UserEdit } from './views/UserEdit';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
+import { UserList } from './views/UserList';
 
-const user = User.buildUser({ name: 'NAME', age: 20 });
-const root = document.getElementById('root');
-// if or ! @end - we now that root exists -> not null
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
-} else {
-  throw new Error('Root element not found');
-}
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  },
+);
+
+users.on('change', () => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
