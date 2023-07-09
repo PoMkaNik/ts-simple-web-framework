2ad573f40dd1bac52c0d53b8bb26ffe0ae554ea6
Roman Dubovik <ip.romandubovik@gmail.com>
2023-07-09 17:26:30 +0300

    Model class was extracted from User

---
+++ src/models/Model.ts
@@ -0,0 +1,70 @@
+import { AxiosPromise, AxiosResponse } from 'axios';
+
+interface ModelAttributes<T> {
+  set(update: T): void;
+  getAll(): T;
+  get<K extends keyof T>(key: K): T[K];
+}
+
+interface Sync<T> {
+  fetch(id: number): AxiosPromise;
+  save(data: T): AxiosPromise;
+}
+
+interface Events {
+  on(eventName: string, callback: () => {}): void;
+  trigger(eventName: string): void;
+}
+
+interface HasId {
+  id?: number;
+}
+
+export class Model<T extends HasId> {
+  constructor(
+    private attributes: ModelAttributes<T>,
+    private events: Events,
+    private sync: Sync<T>,
+  ) {}
+
+  // instanceOfUser.on() -> we will call returned function using get instead of
+  // using simple method to avoid passing excessive attributes to this method
+  get on() {
+    // we changed method to arrow function to 'leave' current this, not events
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+
+  set(update: T): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id');
+    }
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger('save');
+      })
+      .catch(() => {
+        this.trigger('error');
+      });
+  }
+}
