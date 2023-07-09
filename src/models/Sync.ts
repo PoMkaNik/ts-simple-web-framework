e1b0617bb41f7f86e92980362d4974d6cf5b815a
Roman Dubovik <ip.romandubovik@gmail.com>
2023-07-09 13:42:20 +0300

    user class decomposition: sync functionality moved to separate class

---
+++ src/models/Sync.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosPromise } from 'axios';
+
+// constraint
+interface HasId {
+  id?: number;
+}
+
+export class Sync<T extends HasId> {
+  // http://localshost:3000/users <- expected format
+  constructor(public rootUrl: string) {}
+
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rootUrl}/${id}`);
+  }
+
+  save(data: T): AxiosPromise {
+    const { id } = data;
+    // axios({
+    //   url: `${this.rootUrl}/${id ? id : ''}`,
+    //   method: id ? 'PUT' : 'POST',
+    //   data: data,
+    // });
+    if (id) {
+      return axios.put(`${this.rootUrl}/${id}`, data);
+    } else {
+      return axios.post(`${this.rootUrl}`, data);
+    }
+  }
+}
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,16 +1,17 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 
-interface UserProps {
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
 
-type Callback = () => void;
+const rootUrl = 'http://localhost:3000/users';
 
 export class User {
-  public events: Eventing = new Eventing()
+  public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 
   constructor(private data: UserProps) {}
 
@@ -21,25 +22,5 @@ export class User {
   set(update: UserProps): void {
     // Object.assign(this.data, update);
     this.data = { ...this.data, ...update };
-  }
-
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((res: AxiosResponse): void => {
-        this.set(res.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get('id');
-    // axios({
-    //   url: `http://localhost:3000/users/${id ? id : ''}`,
-    //   method: id ? 'PUT' : 'POST',
-    //   data: this.data,
-    // });
-    id
-      ? axios.put(`http://localhost:3000/users/${id}`, this.data)
-      : axios.post(`http://localhost:3000/users`, this.data);
   }
 }
