08a58e94eca5e34f13a72eaa33ba8f0efd1c71fd
Roman Dubovik <ip.romandubovik@gmail.com>
2023-07-09 17:10:50 +0300

    methods added to User class for better UX

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,7 @@
 import { User } from './models/User';
 
 const user = new User({ id: 1 });
+
+user.on('change', () => console.log(user));
+
 user.fetch();
-setTimeout(() => {
-  console.log(user);
-  user.set({ name: 'new-new-name-3' });
-  console.log(user);
-  user.save();
-}, 1000);
-
-// const userNew = new User({ name: 'user5', age: 30 });
-// userNew.save();
-
-// console.log(userNew);
